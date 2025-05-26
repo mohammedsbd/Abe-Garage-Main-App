8b56ad18a03e14b198731c4b98d0c94a7607e623
mohammedsbd <mohammed1112ok@gmail.com>
2025-05-26 01:12:44 -0700

    add query pararms

---
+++ backend/config/db.config.js
@@ -0,0 +1,25 @@
+//import mysql module promise wrapper
+const mysql = require('mysql2/promise');
+//import dotenv to load environment variables
+const dotenv = require('dotenv');
+//load environment variables from .env file
+dotenv.config();
+//create a connection to the database using environment variables
+const dbConfig =({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 10, // Set the maximum number of connections in the pool
+    });
+//create a connection pool to the database
+const pool = mysql.createPool(dbConfig);
+// prepare a function that will execute the sql queries asynchronously
+async function Query(sql, params) {
+    const [rows,fields] = await pool.execute(sql, params);
+    return rows;
+}
+
+ 
+
+
