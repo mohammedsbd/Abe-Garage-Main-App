04dec404cf9bbb11578f5a6963900a5c585fdd71
aaronkefyalew <aaronkefyalew@gmail.com>
2025-06-27 17:39:32 +1100

    add employee from handler clinet side

--- frontend/abe-garage/src/markup/components/Admin/AddEmployeeForm/AddEmployeeForm.jsx
+++ frontend/abe-garage/src/markup/components/Admin/AddEmployeeForm/AddEmployeeForm.jsx
@@ -16,8 +16,88 @@ function AddEmployeeForm(props) {
   const [success, setSuccess] = useState(false);
   const [serverError, setServerError] = useState("");
 
+ const handleSubmit = (e) => {
+    // Prevent the default behavior of the form
+    e.preventDefault();
+    // Handle client side validations  
+    let valid = true; // Flag 
+    // First name is required 
+    if (!employee_first_name) {
+      setFirstNameRequired('First name is required');
+      valid = false;
+    } else {
+      setFirstNameRequired('');
+    }
+    // Email is required
+    if (!employee_email) {
+      setEmailError('Email is required');
+      valid = false;
+    } else if (!employee_email.includes('@')) {
+      setEmailError('Invalid email format');
+    } else {
+      const regex = /^\S+@\S+\.\S+$/;
+      if (!regex.test(employee_email)) {
+        setEmailError('Invalid email format');
+        valid = false;
+      } else {
+        setEmailError('');
+      }
+    }
+    // Password has to be at least 6 characters long
+    if (!employee_password || employee_password.length < 6) {
+      setPasswordError('Password must be at least 6 characters long');
+      valid = false;
+    } else {
+      setPasswordError('');
+    }
+    // If the form is not valid, do not submit 
+    if (!valid) {
+      return;
+    }
+    const formData = {
+      employee_email,
+      employee_first_name,
+      employee_last_name,
+      employee_phone,
+      employee_password,
+      active_employee,
+      company_role_id
+    };
+
+    // Pass the form data to the service 
+    const newEmployee = employeeService.createEmployee(formData);
+    newEmployee.then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        // If Error is returned from the API server, set the error message
+        if (data.error) {
+          setServerError(data.error)
+        } else {
+          // Handle successful response 
+          setSuccess(true);
+          setServerError('')
+          // Redirect to the employees page after 2 seconds 
+          // For now, just redirect to the home page 
+          setTimeout(() => {
+            // window.location.href = '/admin/employees';
+            window.location.href = '/';
+          }, 2000);
+        }
+      })
+      // Handle Catch 
+      .catch((error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+        setServerError(resMessage);
+      });
+  }
+
   
-  return (
+   return (
     <section className="contact-section">
       <div className="auto-container">
         <div className="contact-title">
@@ -27,46 +107,28 @@ function AddEmployeeForm(props) {
           <div className="form-column col-lg-7">
             <div className="inner-column">
               <div className="contact-form">
-                <form>
+                <form onSubmit={handleSubmit}>
                   <div className="row clearfix">
                     <div className="form-group col-md-12">
-                      <input
-                        type="email"
-                        name="employee_email"
-                        placeholder="Employee email"
-                      />
+                      {serverError && <div className="validation-error" role="alert">{serverError}</div>}
+                      <input type="email" name="employee_email" value={employee_email} onChange={event => setEmail(event.target.value)} placeholder="Employee email" />
+                      {emailError && <div className="validation-error" role="alert">{emailError}</div>}
                     </div>
                     <div className="form-group col-md-12">
-                      <input
-                        type="text"
-                        name="employee_first_name"
-                        placeholder="Employee first name"
-                      />
+                      <input type="text" name="employee_first_name" value={employee_first_name} onChange={event => setFirstName(event.target.value)} placeholder="Employee first name" />
+                      {firstNameRequired && <div className="validation-error" role="alert">{firstNameRequired}</div>}
                     </div>
 
                     <div className="form-group col-md-12">
-                      <input
-                        type="text"
-                        name="employee_last_name"
-                        placeholder="Employee last name"
-                        required
-                      />
+                      <input type="text" name="employee_last_name" value={employee_last_name} onChange={event => setLastName(event.target.value)} placeholder="Employee last name" required />
                     </div>
 
                     <div className="form-group col-md-12">
-                      <input
-                        type="text"
-                        name="employee_phone"
-                        placeholder="Employee phone (555-555-5555)"
-                        required
-                      />
+                      <input type="text" name="employee_phone" value={employee_phone} onChange={event => setPhoneNumber(event.target.value)} placeholder="Employee phone (555-555-5555)" required />
                     </div>
 
                     <div className="form-group col-md-12">
-                      <select
-                        name="employee_role"
-                        className="custom-select-box"
-                      >
+                      <select name="employee_role" value={company_role_id} onChange={event => setCompany_role_id(event.target.value)} className="custom-select-box">
                         <option value="1">Employee</option>
                         <option value="2">Manager</option>
                         <option value="3">Admin</option>
@@ -74,31 +136,24 @@ function AddEmployeeForm(props) {
                     </div>
 
                     <div className="form-group col-md-12">
-                      <input
-                        type="password"
-                        name="employee_password"
-                        placeholder="Employee Login password"
-                      />
+                      <input type="password" name="employee_password" value={employee_password} onChange={event => setPassword(event.target.value)} placeholder="Employee password" />
+                      {passwordError && <div className="validation-error" role="alert">{passwordError}</div>}
                     </div>
 
                     <div className="form-group col-md-12">
-                      <button
-                        className="theme-btn btn-style-one"
-                        type="submit"
-                        data-loading-text="Please wait..."
-                      >
-                        <span>Add employee</span>
-                      </button>
+                      <button className="theme-btn btn-style-one" type="submit" data-loading-text="Please wait..."><span>Add employee</span></button>
                     </div>
                   </div>
                 </form>
               </div>
             </div>
           </div>
+
         </div>
       </div>
     </section>
   );
 }
+
 
 export default AddEmployeeForm;
