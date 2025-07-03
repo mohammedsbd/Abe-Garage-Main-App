5bd42ffccd96086bcda69b9d4a601848f4f3129e
mohammedsbd <mohammed1112ok@gmail.com>
2025-07-03 04:15:42 -0700

    chatbot megred

--- frontend/abe-garage/src/App.jsx
+++ frontend/abe-garage/src/App.jsx
@@ -14,6 +14,7 @@ import "./assets/template-assets/css/style.css";
 import "./assets/template-assets/css/responsive.css";
 import "./assets/template-assets/css/color.css";
 import "./assets/styles/custom.css";
+import VivoChatbot from './markup/components/Chatbot/Chatbot.jsx';
 
 
 
@@ -25,6 +26,7 @@ function App() {
   return (
     <>
       <Header />
+      <VivoChatbot />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
---
+++ frontend/abe-garage/src/markup/components/Chatbot/Chatbot.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+
+const VivoChatbot = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://cdn.botpress.cloud/webchat/v3.0/inject.js";
+    script.async = true;
+
+    script.onload = () => {
+      window.botpress?.init({
+        botId: "9885bdc9-4d08-4b55-8e25-4fb86867fffa",
+        clientId: "6b3ca485-4a5e-41aa-bb46-a320256c1ce5",
+        configuration: {
+          version: "v1",
+          botName: "Vivo",
+          botAvatar:
+            "https://files.bpcontent.cloud/2025/07/01/16/20250701162910-U8K086OS.png",
+          fabImage:
+            "https://files.bpcontent.cloud/2025/07/01/16/20250701164014-FTWAA2RK.png",
+          color: "#ef4444",
+          variant: "solid",
+          headerVariant: "solid",
+          themeMode: "light",
+          fontFamily: "ibm",
+          radius: 4,
+          feedbackEnabled: false,
+          footer: "[⚡ by Botpress](https://botpress.com/?from=webchat)",
+        },
+      });
+
+      window.botpress?.on("webchat:ready", () => {
+        window.botpress.open();
+      });
+    };
+
+    document.body.appendChild(script);
+  }, []);
+
+  return null; // No visible UI
+};
+
+export default VivoChatbot;
