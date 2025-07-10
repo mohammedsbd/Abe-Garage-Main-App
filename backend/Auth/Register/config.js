062929e9c7925b3feb63ce0144d4b6462cd0ba15
aaronkefyalew <aaronkefyalew@gmail.com>
2025-07-10 15:21:45 +1100

    called once from server.js to establish DB access.

--- backend/Auth/Register/config.js
+++ backend/Auth/Register/config.js
@@ -6,4 +6,11 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-   
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
