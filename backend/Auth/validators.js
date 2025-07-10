df73feddbe1b74016462542541726dacb57f29f2
aaronkefyalew <aaronkefyalew@gmail.com>
2025-07-10 15:37:43 +1100

    Added validateLogin to ensure email and password are provided

--- backend/Auth/validators.js
+++ backend/Auth/validators.js
@@ -1,2 +1,9 @@
 const { body } = require('express-validator');
 
+const validateRegister = [
+  body('username').notEmpty().withMessage('Username is required'),
+  body('email').isEmail().withMessage('Enter a valid email'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+];
+
+
