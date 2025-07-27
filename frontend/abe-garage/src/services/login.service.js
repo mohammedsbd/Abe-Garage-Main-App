7778da055e0db906fdeede13b42c2676217b1a12
mohammedsbd <mohammed1112ok@gmail.com>
2025-07-27 01:19:40 -0700

    add employee login form and logic

---
+++ frontend/abe-garage/src/services/login.service.js
@@ -0,0 +1,18 @@
+const logIn = async (formData) => {
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
+  };
+
+  console.log("About to send request");
+  console.log(requestOptions.body);
+
+  const response = await fetch(
+    "http://localhost:8000/api/employee/login",
+    requestOptions
+  );
+  return response;
+};
+
+export default { logIn };
